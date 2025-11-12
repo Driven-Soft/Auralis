96083af96f3ca658573f1b1fa1ad43dbcb9602da
Max Hayashi Batista <rm563717@fiap.com.br>
2025-11-12 14:37:02 -0300

    feat: adicionado no home os novos cards (new user, usercard)

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,16 +1,32 @@
 import Hero from "../components/Hero";
+import NewUser from "../components/NewUser";
+import UserCard from "../components/UserCard";
 import Wrapper from "../components/Wrapper";
 
 const Home = () => {
   return (
     <Wrapper>
       <section>
-        <div className="py-12">
+        <div className="pt-4 sm:pt-12 pb-4">
           <Hero
             title="Bem-vindo(a) ao Auralis"
             text="Sua plataforma de bem-estar profissional"
           />
         </div>
+      </section>
+    <div>
+      <img src="/src/assets/images/icons/AuralisLogo.png"
+      className="mx-auto w-18 h-18"
+      alt="" />
+    </div>
+
+      <div className="text-center mt-6 items-center text-3xl pb-8 text-texto-primary font-medium dark:text-texto-secondary"> 
+        <h2>Selecione seu perfil</h2>
+      </div>
+
+      <section className="grid grid-cols-2 w-[90%] md:w-[60%] mx-auto gap-4 md:grid-cols-2 mb-12">
+        <UserCard name="Ana Silva" score={85} />
+        <NewUser />
       </section>
     </Wrapper>
   );
